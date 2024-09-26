9969a323c676c837c61e26a93d375f2c9e287f67
OlhaKatrych <olgak198931@gmail.com>
2024-09-26 13:57:00 +0300

    homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,17 @@ import css from "./HomePage.module.css";
 export default function HomePage() {
   return (
     <main>
-      <div className={css.hero}></div>
+      <div className={css.heroImage}>
+        <div className={css.hero}>
+          <div className={css.textContainer}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <h2 className={css.desc}>
+            You can find everything you want in our catalog
+          </h2>
+          </div>
+          <button className={css.btn}>View Now</button>
+        </div>
+      </div>
     </main>
   );
 }
