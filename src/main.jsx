8f4305bbe6fa07eeac29df719eb8b43e7b373c67
OlhaKatrych <olgak198931@gmail.com>
2024-09-27 16:41:03 +0300

    Loader

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "modern-normalize";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,7 +1,12 @@
 import css from "./CatalogPage.module.css";
+import { icons } from "../../shared/icons/index";
 
 function CatalogPage() {
-  return <div>Catalog</div>;
+  return (
+    <div>
+      Hello
+    </div>
+  );
 }
 
 export default CatalogPage;
